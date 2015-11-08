bf588f1915045ae2a09a1dbf3750b9946a897b55
Brian Sjoberg <brian_sjoberg@yahoo.com>
2015-11-08 10:23:35 -0500

    Removed exception import.

--- ElephantCarpaccio/src/test/java/com/bdd/ElephantCarpaccio/HelloWorldSteps.java
+++ ElephantCarpaccio/src/test/java/com/bdd/ElephantCarpaccio/HelloWorldSteps.java
@@ -2,7 +2,6 @@ package com.bdd.ElephantCarpaccio;
 
 import org.junit.Assert;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
